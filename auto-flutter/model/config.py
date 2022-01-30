4f8e739ed36bd124b35479c324629643d0d5ce41
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-01-30 15:23:16 -0300

    Save config and fixes while reading

--- auto-flutter/model/config.py
+++ auto-flutter/model/config.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
-import json
-from pathlib import Path, PurePosixPath
-from typing import Dict
+from json import load as json_load, dump as json_dump
+from pathlib import Path, PurePath, PurePosixPath
 from appdirs import user_config_dir
 from core.os import OS
 
@@ -19,24 +18,53 @@ class Config:
         self.firebase: PurePosixPath = PurePosixPath("firebase")
         self.firebase_standalone: bool = False
 
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        return (
+            "Config("
+            + "flutter="
+            + str(self.flutter)
+            + ", "
+            + "firebase="
+            + str(self.firebase)
+            + ", "
+            + "firebase-standalone="
+            + str(self.firebase_standalone)
+            + ")"
+        )
+
     def load(self) -> bool:
         filepath = Config.get_config_file_path()
         if not filepath.exists():
             return False
         file = open(filepath, mode="r", encoding="utf-8")
-        parsed = json.load(file)
+        parsed = json_load(file)
         file.close()
-        if not parsed is Dict:
-            raise TypeError("Parsed config is invalid")
+
         if "flutter" in parsed and len(parsed["flutter"]) != 0:
-            self.flutter = OS.machine_to_posix_path(parsed["flutter"])
+            self.flutter = OS.machine_to_posix_path(PurePath(parsed["flutter"]))
 
         if "firebase" in parsed and len(parsed["firebase"]) != 0:
-            self.firebase = OS.machine_to_posix_path(parsed["firebase"])
+            self.firebase = OS.machine_to_posix_path(PurePath(parsed["firebase"]))
 
         if "firebase-standalone" in parsed and parsed["firebase-standalone"] is bool:
             self.firebase_standalone = bool(parsed["firebase-standalone"])
         return True
+
+    def save(self):
+        filepath = Config.get_config_file_path()
+        if not filepath.exists():
+            filepath.parent.mkdir(parents=True, exist_ok=True)
+        output = {
+            "flutter": str(self.flutter),
+            "firebase": str(self.firebase),
+            "firebase-standalone": self.firebase_standalone,
+        }
+        file = open(filepath, mode="wt", encoding="utf-8")
+        json_dump(output, file, indent=2)
+        file.close()
 
     @staticmethod
     def get_config_file_path() -> Path:
