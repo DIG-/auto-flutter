d724a983e8669b4c664d18b58f1411e2cc10ca67
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-01-30 20:34:13 -0300

    New Args class

--- auto-flutter/core/arguments.py
+++ auto-flutter/core/arguments.py
@@ -1,3 +1,17 @@
-from typing import Dict
+from __future__ import annotations
+from ast import arguments, operator
+from operator import itemgetter
+from typing import Any, Dict, Optional, Tuple
 
-Args = Dict[str, str]
+
+class Args(Tuple[str, str, Optional[str]]):
+    def __new__(cls: type[Args], argument: str, value: Optional[str]) -> Args:
+        argument = argument.strip()
+        key = argument.lstrip("-").lower()
+        if not argument.startswith("-"):
+            key = "-#-" + key
+        return super().__new__(Args, (key, argument, value))
+
+    key: str = property(itemgetter(0))
+    argument: str = property(itemgetter(1))
+    value: Optional[str] = property(itemgetter(2))
