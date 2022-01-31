727f8ae750b4448ceafb5aef269eab35aaa355eb
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-01-31 17:54:23 -0300

    Added type for Flavor, to avoid misleading

--- auto-flutter/model/build_config.py
+++ auto-flutter/model/build_config.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import Dict, List, Optional
 from ..model.build_type import BuildType
+from ..model.flavor import Flavor
 
 
 class BuildRunBefore(Enum):
@@ -21,9 +22,9 @@ class BuildConfig:
 
 
 class BuildConfigFlavored(BuildConfig):
-    flavored: Optional[Dict[str, BuildConfig]]
+    flavored: Optional[Dict[Flavor, BuildConfig]]
 
-    def get_build_param(self, flavor: Optional[str]) -> str:
+    def get_build_param(self, flavor: Optional[Flavor]) -> str:
         output = ""
         if not self.build_param is None:
             output += self.build_param
@@ -35,7 +36,9 @@ class BuildConfigFlavored(BuildConfig):
                 output += flavored_param
         return output
 
-    def get_run_before(self, flavor: Optional[str]) -> Dict[BuildRunBefore, List[str]]:
+    def get_run_before(
+        self, flavor: Optional[Flavor]
+    ) -> Dict[BuildRunBefore, List[str]]:
         output: Dict[BuildRunBefore, List[str]] = dict()
         if not self.run_before is None:
             output = self.run_before
@@ -51,7 +54,7 @@ class BuildConfigFlavored(BuildConfig):
                     output[key] = [*output[key], *value]
         return output
 
-    def get_output(self, flavor: Optional[str], type: BuildType) -> Optional[str]:
+    def get_output(self, flavor: Optional[Flavor], type: BuildType) -> Optional[str]:
         if not flavor is None and flavor in self.flavored:
             from_flavor = self.flavored[flavor].get_output(type)
             if not from_flavor is None:
