9780209cc26d9b9c0ddd55dfcc0fadd2342255f7
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-01-30 21:48:39 -0300

    Added Option to TaskIdentity

--- auto-flutter/core/task.py
+++ auto-flutter/core/task.py
@@ -2,18 +2,23 @@ from __future__ import annotations
 from abc import ABCMeta, abstractclassmethod
 from operator import itemgetter
 from typing import Any, Callable, List, Optional, Tuple
-from core.arguments import Args
+from core.arguments import Args, Option
 
 
-class TaskIdentity(Tuple[str, str, Callable[[], Any]]):
+class TaskIdentity(Tuple[str, str, List[Option], Callable[[], Any]]):
     def __new__(
-        cls: type[TaskIdentity], id: str, name: str, creator: Callable[[], Task]
+        cls: type[TaskIdentity],
+        id: str,
+        name: str,
+        options: List[Option],
+        creator: Callable[[], Task],
     ) -> TaskIdentity:
-        return super().__new__(TaskIdentity, (id, name, creator))
+        return super().__new__(TaskIdentity, (id, name, options, creator))
 
     id: str = property(itemgetter(0))
     name: str = property(itemgetter(1))
-    creator: Callable[[], Task] = property(itemgetter(2))
+    options: List[Option] = property(itemgetter(2))
+    creator: Callable[[], Task] = property(itemgetter(3))
 
 
 class Task(metaclass=ABCMeta):
