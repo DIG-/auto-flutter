2e9f674a154a465108e1cc44ce1038195dab996c
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-02 11:40:14 -0300

    Create _Ensure.not_none() to raise when value is none

--- auto-flutter/core/utils.py
+++ auto-flutter/core/utils.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from abc import ABCMeta
 from typing import (
     Callable,
     Iterable,
@@ -30,3 +31,15 @@ class _Iterable:
 
     def flatten(iterable: Iterable[Iterable[T]]) -> List[T]:
         return [item for sublist in iterable for item in sublist]
+
+
+class _Ensure(metaclass=ABCMeta):
+    T = TypeVar("T")
+
+    def not_none(input: Optional[T], name: Optional[str] = None) -> T:
+        if input is None:
+            if name is None:
+                raise AssertionError("Field require valid value")
+            else:
+                raise AssertionError("Field `{}` require valid value".format(name))
+        return input
