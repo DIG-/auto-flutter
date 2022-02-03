06faa389ebc18282b8cca48b0ea70b6e62184427
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-02 22:27:16 -0300

    Using result message in help

--- auto-flutter/task/help.py
+++ auto-flutter/task/help.py
@@ -1,3 +1,4 @@
+from email import message
 from ..core.arguments import Option, Args
 from ..core.task import Task, TaskIdentity, TaskResult
 from ..core.utils import _Iterable
@@ -5,6 +6,7 @@ from ..task._resolver import TaskResolver
 from sys import argv as sys_argv
 from typing import Dict, List
 from ..task.project_read import ProjectRead
+from ..core.string_builder import SB
 
 
 class Help(Task):
@@ -26,49 +28,65 @@ class Help(Task):
         return [ProjectRead.identity_skip.id]
 
     def execute(self, args: Args) -> TaskResult:
-        self.show_header()
+        builder = SB()
+        self.show_header(builder)
         if "task" in args:
             task_id = args["task"].value
             if (not task_id is None) and len(task_id) > 0:
                 identity = TaskResolver.find_task(task_id)
                 if not identity is None:
-                    self.show_task_options(identity)
-                    return TaskResult(args)
+                    self.show_task_options(identity, builder)
+                    return TaskResult(args, message=builder.str())
                 else:
-                    self.print('Task "{}" not found\n'.format(task_id))
-        self.print("Default tasks:")
+                    builder.append("Task ").append(task_id, SB.Color.CYAN, True).append(
+                        " not found\n"
+                    )
+        builder.append("Default tasks:\n")
         for task in Help.reduce_indexed_task_into_list(self._default):
-            self.show_task_name(task)
-        return TaskResult(args)
+            self.show_task_name(task, builder)
+        return TaskResult(args, message=builder.str())
 
-    def show_header(self):
-        self.print("Usage:\t{} TASK [options]\n".format(sys_argv[0]))
+    def show_header(self, builder: SB):
+        builder.append("Usage:\t").append(sys_argv[0]).append(
+            " TASK", SB.Color.CYAN, True
+        ).append(" [options]\n", SB.Color.MAGENTA)
 
-    def show_task_options(self, task: TaskIdentity):
-        self.print("Task:\t{}".format(task.id))
-        self.print(task.name)
+    def show_task_options(self, task: TaskIdentity, builder: SB):
+        builder.append("Task:\t").append(task.id, SB.Color.CYAN, True, end="\n").append(
+            task.name
+        )
         options_mapped = map(
             lambda task: task.identity.options, TaskResolver.resolve(task.creator())
         )
         options = _Iterable.flatten(options_mapped)
         if len(options) == 0:
-            self.print("\nThis task does not have options")
+            builder.append("\nThis task does not have options")
         else:
-            self.print("\nOptions:")
+            builder.append("\nOptions:")
             for option in options:
-                has_value = " <value>" if option.has_value else ""
-                separator = (
-                    ", "
-                    if (not option.short is None) and (not option.long is None)
-                    else ""
-                )
-                has_short = "-" + option.short if not option.short is None else ""
-                has_long = "--" + option.long if not option.long is None else ""
-                final = has_short + separator + has_long + has_value
-                self.print("{:20s}\t{}".format(final, option.description))
+                length = 0
+                if not option.short is None:
+                    builder.append("-" + option.short, SB.Color.MAGENTA)
+                    length += len(option.short) + 1
+                    if not option.long is None:
+                        builder.append(", ")
+                        length += 1
+                if not option.long is None:
+                    builder.append("--" + option.long, SB.Color.MAGENTA)
+                    length += len(option.long) + 2
+                if option.has_value:
+                    builder.append(" <value>", SB.Color.MAGENTA, True)
+                    length += 8
 
-    def show_task_name(self, task: TaskIdentity):
-        self.print("  {}\t{}".format(task.id, task.name))
+                if length < 20:
+                    builder.append(" " * (20 - length))
+                builder.append("\t").append(option.description)
+
+    def show_task_name(self, task: TaskIdentity, builder: SB):
+        builder.append("  ").append(task.id, SB.Color.CYAN, True)
+        if len(task.id) < 8:
+            builder.append(" " * (8 - len(task.id)))
+        builder.append("\t").append(task.name, end="\n")
 
     def reduce_indexed_task_into_list(
         tasks: Dict[str, TaskIdentity]
