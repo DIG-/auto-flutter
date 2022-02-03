76a59487512fc48c8f135b0441aece36ad8e58f8
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-03 13:27:19 -0300

    Passing arguments from task doctor directly to exec

--- auto-flutter/task/flutter.py
+++ auto-flutter/task/flutter.py
@@ -1,14 +1,15 @@
-from aifc import Error
-from pprint import pprint
-from typing import List, Optional, Tuple
+from typing import List, Optional
 from ..core.task import Task
 from ..core.process.process import Process
 from ..task.project_read import ProjectRead
 from ..model.config import Config
+from ..core.arguments import OptionAll
 
 
 class Flutter(Task):
-    identity = Task.Identity("exec", "Run flutter command", [], lambda: Flutter())
+    identity = Task.Identity(
+        "exec", "Run flutter command", [OptionAll()], lambda: Flutter()
+    )
 
     class Error(ChildProcessError):
         ...
@@ -17,11 +18,13 @@ class Flutter(Task):
         self,
         project: bool = True,
         command: Optional[List[str]] = None,
+        command_append_args: bool = False,
         output: bool = False,
     ) -> None:
         super().__init__()
         self._project: bool = project
         self._command: Optional[List[str]] = command
+        self._command_args: bool = command_append_args
         self._output: bool = output
 
     def require(self) -> List[Task.ID]:
@@ -32,6 +35,8 @@ class Flutter(Task):
     def execute(self, args: Task.Args) -> Task.Result:
         flutter = Config.instance().flutter
         if not self._command is None and len(self._command) > 0:
+            if self._command_args:
+                self._command.extend(map(lambda x: x.argument, args.values()))
             p = Process.create(flutter, self._command, lambda x: self.print(x))
             p.run()
             if self._output:
@@ -50,5 +55,8 @@ class Flutter(Task):
 
 
 FlutterDoctor = Task.Identity(
-    "doctor", "Run flutter doctor", [], lambda: Flutter(False, ["doctor"])
+    "doctor",
+    "Run flutter doctor",
+    [OptionAll()],
+    lambda: Flutter(False, ["doctor"], command_append_args=True),
 )
