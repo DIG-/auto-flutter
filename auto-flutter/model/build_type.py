9b81ddbf1fe4215f802d2915d53f0c9b699df800
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-04 09:44:57 -0300

    Added FlutterBuildType to be used in build task

--- auto-flutter/model/build_type.py
+++ auto-flutter/model/build_type.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from typing import Optional
+from ..core.utils import _Iterable
 
 
 class BuildType(Enum):
@@ -6,3 +8,31 @@ class BuildType(Enum):
     APK = "apk"
     BUNDLE = "aab"
     IPA = "ipa"
+
+    def to_FlutterBuildType(self) -> "FlutterBuildType":
+        found = _Iterable.first_or_none(
+            FlutterBuildType.__iter__, lambda other: other.name == self.name
+        )
+        if found is None:
+            raise AttributeError("Can not find FlutterBuildType from BuildType")
+        return found
+
+
+class FlutterBuildType(Enum):
+    AAR = "aar"
+    APK = "apk"
+    BUNDLE = "appbundle"
+    IPA = "ipa"
+
+    def to_BuildType(self) -> BuildType:
+        found = _Iterable.first_or_none(
+            BuildType.__iter__, lambda other: other.name == self.name
+        )
+        if found is None:
+            raise AttributeError("Can not find BuildType from FlutterBuildType")
+        return found
+
+    def from_str(string: str) -> Optional["FlutterBuildType"]:
+        return _Iterable.first_or_none(
+            FlutterBuildType.__iter__, lambda it: it.value == string
+        )
