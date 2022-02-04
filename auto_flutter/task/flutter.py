595c2230682bded24605dd012e505bb49afd64a0
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-04 12:57:24 -0300

    Unified Flutter task execute

--- auto_flutter/task/flutter.py
+++ auto_flutter/task/flutter.py
@@ -1,9 +1,11 @@
+from ast import arguments
 from typing import List, Optional
 from ..core.task import Task
 from ..core.process.process import Process
 from ..task.project_read import ProjectRead
 from ..model.config import Config
-from ..core.arguments import OptionAll
+from ..core.arguments import Arg, OptionAll
+from ..core.string_builder import SB
 
 
 class Flutter(Task):
@@ -19,13 +21,17 @@ class Flutter(Task):
         project: bool = True,
         command: Optional[List[str]] = None,
         command_append_args: bool = False,
-        output: bool = False,
+        output_running: bool = True,
+        output_end: bool = False,
+        output_arg: bool = False,
     ) -> None:
         super().__init__()
         self._project: bool = project
         self._command: Optional[List[str]] = command
         self._command_args: bool = command_append_args
-        self._output: bool = output
+        self._output_running: bool = output_running
+        self._output_end: bool = output_end
+        self._output_arg: bool = output_arg
 
     def require(self) -> List[Task.ID]:
         if self._project:
@@ -34,33 +40,33 @@ class Flutter(Task):
 
     def execute(self, args: Task.Args) -> Task.Result:
         flutter = Config.instance().flutter
+        writer = None if not self._output_running else lambda x: self.print(x)
+
+        if self._output_end and self._output_running:
+            self.print(
+                SB()
+                .append("[!] Running command will show output twice", SB.Color.YELLOW)
+                .str()
+            )
+
         if not self._command is None and len(self._command) > 0:
             if self._command_args:
                 self._command.extend(args.to_command_arg())
-            p = Process.create(
-                flutter, arguments=self._command, writer=lambda x: self.print(x)
-            )
-            p.run()
-            if self._output:
-                return Task.Result(
-                    args,
-                    error=Flutter.Error(p.exit_code, p.output),
-                    success=p.exit_code == 0,
-                )
-            else:
-                return Task.Result(
-                    args,
-                    success=p.exit_code == 0,
-                )
-        p = Process.create(
-            flutter, arguments=args.to_command_arg(), writer=lambda x: self.print(x)
-        )
+            p = Process.create(flutter, arguments=self._command, writer=writer)
+        else:
+            p = Process.create(flutter, arguments=args.to_command_arg(), writer=writer)
         output = p.try_run()
+
+        if self._output_end:
+            self.print(p.output)
+
+        if self._output_arg:
+            args["output"] = Arg("output", p.output)
+
         if isinstance(output, BaseException):
             return Task.Result(args, error=output)
         return Task.Result(
             args,
-            error=Flutter.Error(p.exit_code, p.output) if self._output else None,
             success=output,
         )
 
@@ -69,5 +75,5 @@ FlutterDoctor = Task.Identity(
     "doctor",
     "Run flutter doctor",
     [OptionAll()],
-    lambda: Flutter(False, ["doctor"], command_append_args=True),
+    lambda: Flutter(project=False, command=["doctor"], command_append_args=True),
 )
