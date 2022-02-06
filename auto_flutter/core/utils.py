53204aa37b1d863227a530b25ff6fdc705655b3b
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-06 15:51:40 -0300

    Added _Ensure.instance, which return if instance is valid

--- auto_flutter/core/utils.py
+++ auto_flutter/core/utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from abc import ABCMeta
-from typing import Callable, Dict, Iterable, List, Optional, Type, TypeVar
+from typing import Any, Callable, Dict, Iterable, List, Optional, Type, TypeVar
 
 
 class _Iterable(metaclass=ABCMeta):
@@ -84,14 +84,38 @@ class _Ensure(metaclass=ABCMeta):
                 raise AssertionError("Field `{}` require valid value".format(name))
         return input
 
-    def type(input: Optional[T], cls: Type[T], name: Optional[str] = None):
+    def type(
+        input: Optional[T], cls: Type[T], name: Optional[str] = None
+    ) -> Optional[T]:
         if input is None:
-            return
+            return None
         if isinstance(input, cls):
-            return
+            return input
         if name is None:
-            raise AssertionError("Field must be instance of `{}`".format(cls.__name__))
+            raise AssertionError(
+                "Field must be instance of `{}`, but `{}` was used".format(
+                    cls.__name__, type(input)
+                )
+            )
         else:
             raise AssertionError(
-                "Field `{}` must be instance of `{}`".format(name, cls.__name__)
+                "Field `{}` must be instance of `{}`, but `{}` was used".format(
+                    name, cls.__name__, type(input)
+                )
+            )
+
+    def instance(input: Any, cls: Type[T], name: Optional[str] = None) -> T:
+        if not input is None and isinstance(input, cls):
+            return input
+        if name is None:
+            raise AssertionError(
+                "Field must be intance of `{}`, but `{}` was used".format(
+                    cls.__name__, type(input)
+                )
+            )
+        else:
+            raise AssertionError(
+                "Field {} must be intance of `{}`, but `{}` was used".format(
+                    name, cls.__name__, type(input)
+                )
             )
