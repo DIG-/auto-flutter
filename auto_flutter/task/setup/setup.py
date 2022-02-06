bc6dc3640a326abf114c4dbd74e1abe007ab26a8
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-06 18:19:28 -0300

    Adjust Setup task

--- auto_flutter/task/setup/setup.py
+++ auto_flutter/task/setup/setup.py
@@ -1,42 +1,44 @@
-from typing import List
+from typing import Final, List
 
 from ...model.argument import Args
 from ...model.config import Config
-from ...model.task import Task, TaskId, TaskIdentity, TaskResult
+from ...model.task import Task
 from .check import SetupCheck
 from .edit import SetupEdit
 
 
 class Setup(Task):
-    identity: TaskIdentity = TaskIdentity(
+    identity: Task.Identity = Task.Identity(
         "setup",
         "Edit global config",
         [],
         lambda: Setup(),
     )
 
-    def require(self) -> List[TaskId]:
+    def require(self) -> List[Task.ID]:
         return [SetupEdit.identity.id]
 
     def describe(self, args: Args) -> str:
-        if "show" in args:
+        if args.contains(SetupEdit.option_show):
             return "Showing current config"
-        elif "check" in args:
+        elif args.contains(SetupEdit.option_check):
             return "Checking current config"
         return "Saving config to file"
 
-    def execute(self, args: Args) -> TaskResult:
-        if "show" in args:
-            return TaskResult(args, message=str(Config.instance()))
-        elif "check" in args:
+    def execute(self, args: Args) -> Task.Result:
+        if args.contains(SetupEdit.option_show):
+            return Task.Result(args, message=str(Config.instance()))
+
+        elif args.contains(SetupEdit.option_check):
             from ...core.task.manager import TaskManager
 
-            TaskManager.instance().add(SetupCheck(flutter=True, skip_on_failure=True))
-            TaskManager.instance().add(SetupCheck(firebase=True, skip_on_failure=True))
+            manager: Final = TaskManager.instance()
+            manager.add(SetupCheck(firebase=True, skip_on_failure=True))
+            manager.add(SetupCheck(flutter=True, skip_on_failure=True))
             return Task.Result(args)
 
         try:
             Config.instance().save()
         except BaseException as error:
-            return TaskResult(args, error, success=False)
-        return TaskResult(args)
+            return Task.Result(args, error, success=False)
+        return Task.Result(args)
