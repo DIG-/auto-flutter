be2908811e0b5fb26d0cb66d55ad25d6c2f13f52
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-06 15:54:22 -0300

    Removed SerializableEnum, looks impossible to work like intended

--- auto_flutter/model/_serializable.py
+++ auto_flutter/model/_serializable.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
 from abc import ABCMeta, abstractclassmethod
-from enum import Enum
-from typing import Any, Dict, Final, Generic, List, Optional, TypeVar, Union
+from typing import Dict, Generic, List, Optional, TypeVar, Union
 
 T = TypeVar("T")
 
@@ -17,18 +16,3 @@ class Serializable(Generic[T], metaclass=ABCMeta):
     @abstractclassmethod
     def from_json(json: Serializable.Json) -> Optional[T]:
         raise NotImplementedError("from_json is not implemented")
-
-
-E = TypeVar("E", bound=Enum)
-
-
-class SerializableEnum(Serializable[E]):
-    def __init__(self, enum: E) -> None:
-        super().__init__()
-        self.enum: E = enum
-
-    def __getattribute__(self, __name: str) -> Any:
-        if __name in ("to_json", "from_json"):
-            return object.__getattribute__(self, __name)
-        enum: Final[E] = object.__getattribute__(self, "enum")
-        return enum.__getattribute__(__name)
