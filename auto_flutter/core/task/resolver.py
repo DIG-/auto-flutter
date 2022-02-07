9a955457781ee41823f68dd20de2dba7f44e4077
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-07 19:25:35 -0300

    Improved TaskResolver to remove duplicated unnecessary task

--- auto_flutter/core/task/resolver.py
+++ auto_flutter/core/task/resolver.py
@@ -1,11 +1,11 @@
 from collections import deque
-from queue import Queue
-from typing import Deque, Optional
+from typing import Deque, Final, List, Optional
 
 from ...model.task import Task
 
 
 class TaskResolver:
+    """
     def resolve(task: Task) -> Deque[Task]:
         output: Deque[Task] = deque()
         temp = Queue()
@@ -19,6 +19,47 @@ class TaskResolver:
                     raise LookupError('Task not found "{}"'.format(required))
                 temp.put(found.creator())
         return output
+    """
+
+    def resolve(task: Task) -> Deque[Task]:
+        temp = TaskResolver.__resolve_dependencies(task)
+        temp.reverse()
+        temp = TaskResolver.__clear_repeatable(temp)
+        output: Deque[Task] = deque()
+        for identity in temp:
+            output.appendleft(identity.creator())
+        return output
+
+    def __resolve_dependencies(task: Task) -> List[Task.Identity]:
+        temp: List[Task.Identity] = [Task.Identity("-#-#-", "", [], lambda: task, True)]
+        i = 0
+        while i < len(temp):
+            current: Final = temp[i]
+            _task: Final = current.creator()
+            for id in _task.require():
+                identity = TaskResolver.find_task(id)
+                if identity is None:
+                    raise LookupError('Task not found "{}"'.format(id))
+                j = i + 1
+                temp[j:j] = [identity]
+            i += 1
+        return temp
+
+    def __clear_repeatable(items: List[Task.Identity]) -> List[Task.Identity]:
+        i = 0
+        while i < len(items):
+            current: Final = items[i]
+            j = i + 1
+            while j < len(items):
+                it: Final = items[j]
+                if it.allow_more:
+                    pass
+                elif it.id == current.id:
+                    del items[j]
+                    continue
+                j += 1
+            i += 1
+        return items
 
     def find_task(id: Task.ID) -> Optional[Task.Identity]:
         from ...task._list import task_list, user_task
