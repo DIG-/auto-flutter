546dbaca557257faadc834b57cdfd667a5211fb7
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-07 11:40:42 -0300

    Changed order of format to first use extras

--- auto_flutter/core/string/formatter.py
+++ auto_flutter/core/string/formatter.py
@@ -41,9 +41,9 @@ class StringFormatter:
             arg: str = option
             if arg.isnumeric():
                 arg = "-" + arg
-            parsed = args.get_value(arg)
+            parsed = _Dict.get_or_none(args_extras, arg)
             if parsed is None:
-                parsed = _Dict.get_or_none(args_extras, arg)
+                parsed = args.get_value(arg)
         elif source in ("env", "ENV", "Env"):
             parsed = _Dict.get_or_none(environ, option)
         else:
