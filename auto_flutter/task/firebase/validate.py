3be1b119a0fbb7e8de37f689e6ba464eb07328f8
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-08 10:04:32 -0300

    Implemented FirebaseValidate

--- auto_flutter/task/firebase/validate.py
+++ auto_flutter/task/firebase/validate.py
@@ -1,5 +1,7 @@
-from typing import List
+from typing import Final, List
 
+from ...model.platform import BuildType, MergePlatformConfigFlavored
+from ...model.project import Project
 from ...model.task import Task
 from ..flutter.build.config import FlutterBuildConfig
 from ..project.read import ProjectRead
@@ -13,8 +15,28 @@ class FirebaseBuildValidate(Task):
         lambda: FirebaseBuildValidate(),
     )
 
+    ARG_FIREBASE_GOOGLE_ID: Final = "FIREBASE_CONFIG_GOOGLE_ID"
+
     def require(self) -> List[Task.ID]:
         return [ProjectRead.identity.id, FlutterBuildConfig.identity.id]
 
     def execute(self, args: Task.Args) -> Task.Result:
+        flavor: Final = args.get_value(FlutterBuildConfig.ARG_FLAVOR)
+        build_type: Final = BuildType.from_flutter(
+            args.get_value(FlutterBuildConfig.ARG_BUILD_TYPE)
+        )
+        project: Final = Project.current
+        config = MergePlatformConfigFlavored(
+            project.get_platform_config(Project.Platform.DEFAULT),
+            project.get_platform_config(build_type.platform),
+        )
+        id = config.get_extra(flavor, "google-id")
+        if id is None or len(id) <= 0:
+            return Task.Result(
+                args,
+                error=ValueError("google-id not found in aflutter.json"),
+                success=False,
+            )
+
+        args.add_arg(FirebaseBuildValidate.ARG_FIREBASE_GOOGLE_ID, id)
         return Task.Result(args)
