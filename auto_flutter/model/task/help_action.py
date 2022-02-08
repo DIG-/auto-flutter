fbf0c5e0707e09140b73e6ed3e4138613aaebc7e
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-08 14:00:59 -0300

    Since Action looks toosimilar with common Task, they will be handled as task

--- auto_flutter/model/task/help_action.py
+++ auto_flutter/model/task/help_action.py
@@ -1,42 +1,12 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
-from argparse import Action
-from operator import itemgetter
-from typing import Callable, List, Optional, Tuple, TypeVar
+from abc import ABC, abstractclassmethod
+from typing import List
 
-from ...core.utils import _Iterable
 from .task import Task
 
 
 class HelpAction(ABC):
-    T = TypeVar("T")
-
-    class Action(Tuple[str, List[Task.Option]]):
-        def __new__(
-            cls: type[Action], action: str, options: List[Task.Option]
-        ) -> Action:
-            return super().__new__(Action, (action, options))
-
-        action: str = property(itemgetter(0))
-        options: List[Task.Option] = property(itemgetter(1))
-
-    @staticmethod
-    def _resolve_options_for_task(task: Task) -> List[Task.Option]:
-        from ...core.task.resolver import TaskResolver
-
-        return _Iterable.flatten(
-            map(lambda task: task.identity.options, TaskResolver.resolve(task))
-        )
-
-    @classmethod
-    def _get_action(self, args: Task.Args, decoder: Callable[[str], T]) -> Optional[T]:
-        action = args.get_value("-1")
-        if action is None:
-            return None
-        return decoder(action)
-
-    @classmethod
-    @abstractmethod
-    def actions(self) -> List[Action]:
+    @abstractclassmethod
+    def actions(self) -> List[Task.Identity]:
         raise NotImplementedError()
