5409cad29716e5be75fb539c85c881384c8794f1
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-09 18:01:16 -0300

    Added _Gettable[T], to sobre features like _Lazy

--- auto_flutter/core/utils/__init__.py
+++ auto_flutter/core/utils/__init__.py
@@ -4,3 +4,4 @@ from ._enum import _Enum
 from ._if import _If
 from ._iterable import _Iterable
 from ._raise import _Raise
+from .lazy import _Dynamically, _Lazy, _NotLazy
---
+++ auto_flutter/core/utils/lazy.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from abc import abstractclassmethod
+from typing import Callable, Final, Generic, Optional, TypeVar
+
+T = TypeVar("T")
+
+
+class _Gettable(Generic[T]):
+    @abstractclassmethod
+    def get(self) -> T:
+        raise NotImplementedError()
+
+    def __get(self) -> T:
+        return self.get()
+
+    value: T = property(__get)
+
+
+class _NotLazy(_Gettable[T]):
+    def __init__(self, item: T) -> None:
+        super().__init__()
+        self.__item: Final[T] = item
+
+    def get(self) -> T:
+        return self.__item
+
+
+class _Lazy(_Gettable[T]):
+    def __init__(self, creator: Callable[[], T]) -> None:
+        super().__init__()
+        self.__creator: Final[Callable[[], T]] = creator
+        self.__item: Optional[T] = None
+
+    def get(self) -> T:
+        if self.__item is None:
+            self.__item = self.__creator()
+        return self.__item
+
+
+class _Dynamically(_Gettable[T]):
+    def __init__(self, creator: Callable[[], T]) -> None:
+        super().__init__()
+        self.__creator: Final[[Callable[[], T]]] = creator
+
+    def get(self) -> T:
+        return self.__creator()
