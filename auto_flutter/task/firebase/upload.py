d41cb6f47ae36180ac659546dad3d1e9d858dbbb
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-09 18:22:01 -0300

    First try of task Firebase to upload

--- auto_flutter/task/firebase/upload.py
+++ auto_flutter/task/firebase/upload.py
@@ -1,7 +1,13 @@
-from typing import List
+from pathlib import Path, PurePosixPath
+from typing import Final, List
 
+from ...core.os import OS
+from ...core.process import Process
+from ...model.config import Config
 from ...model.task import Task
 from ..flutter.build.stub import FlutterBuildStub
+from ._const import FIREBASE_DISABLE_INTERACTIVE_MODE, FIREBASE_ENV
+from .check import FirebaseCheck
 from .validate import FirebaseBuildValidate
 
 
@@ -14,7 +20,43 @@ class FirebaseBuildUpload(Task):
     )
 
     def require(self) -> List[Task.ID]:
-        return [FirebaseBuildValidate.identity.id, FlutterBuildStub.identity.id]
+        return [
+            FirebaseBuildValidate.identity.id,
+            FirebaseCheck.identity.id,
+            FlutterBuildStub.identity.id,
+        ]
 
     def execute(self, args: Task.Args) -> Task.Result:
-        return Task.Result(args)
+        filename: Final = args.get_value("output")
+        if filename is None or len(filename) <= 0:
+            return Task.Result(
+                args, AssertionError("Previous task does not have output")
+            )
+
+        file: Path = Path(OS.posix_to_machine_path(PurePosixPath(filename)))
+        if not file.exists():
+            return Task.Result(
+                args, FileNotFoundError("Output not found: {}".format(str(file)))
+            )
+
+        file = file.absolute()
+        google_id: str = args.get_value(FirebaseBuildValidate.ARG_FIREBASE_GOOGLE_ID)
+        if google_id is None or len(google_id) <= 0:
+            return Task.Result(args, AssertionError("Google app id not found"))
+
+        p = Process.create(
+            Config.instance().firebase,
+            arguments=[
+                FIREBASE_DISABLE_INTERACTIVE_MODE.value,
+                "appdistribution:distribute",
+                str(file),
+                "--app",
+                google_id,
+            ],
+            environment=FIREBASE_ENV.value,
+        )
+        output = p.try_run()
+        if isinstance(output, BaseException):
+            return Task.Result(args, error=output)
+
+        return Task.Result(args, success=output)
