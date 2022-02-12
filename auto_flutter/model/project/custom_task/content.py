bcecddf6fd3b5c4192738f9b4069a18de066a672
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-12 13:40:24 -0300

    Updated CustomTaskContent

--- auto_flutter/model/project/custom_task/content.py
+++ auto_flutter/model/project/custom_task/content.py
@@ -1,15 +1,36 @@
 from __future__ import annotations
 
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional
 
-from ....core.json import Serializable
+from ....core.json import *
+from ....core.utils import _Ensure
 
 
-class CustomTaskContent(Dict[str, Union[str, List]], Serializable["CustomTaskContent"]):
+class CustomTaskContent(Serializable["CustomTaskContent"]):
+    def __init__(self, command: str, args: Optional[List[str]]) -> None:
+        self.command: str = _Ensure.instance(command, str, "command")
+        self.args: Optional[List[str]] = args
+
+    @classmethod
     def to_json(self) -> Serializable.Json:
-        return self
+        return {
+            "command": self.command,
+            "args": None
+            if self.args is None
+            else _JsonEncode.encode_list(self.args, lambda x: x),
+        }
 
+    @staticmethod
     def from_json(json: Serializable.Json) -> Optional[CustomTaskContent]:
         if not isinstance(json, Dict):
             return None
-        return CustomTaskContent(json)
+        command: Optional[str] = None
+        args: Optional[List[str]] = None
+        for key, value in json.items():
+            if not isinstance(key, str):
+                continue
+            if key == "command":
+                command = _JsonDecode.decode(value, str)
+            elif key == "args":
+                args = _JsonDecode.decode_list(value, str)
+        return CustomTaskContent(command, args)
