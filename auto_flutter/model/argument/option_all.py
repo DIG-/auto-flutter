8ff61ae6b73e38153d346052f98677beabc6aa7e
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-12 14:33:37 -0300

    Remvoe Tuple inheritance from Option and OptionAll

--- auto_flutter/model/argument/option_all.py
+++ auto_flutter/model/argument/option_all.py
@@ -1,18 +1,15 @@
 from __future__ import annotations
 
-from typing import Optional
-
 from .option import Option
 
 
 class OptionAll(Option):
-    def __new__(cls: type[OptionAll], description: Optional[str] = None) -> OptionAll:
-        return super().__new__(
-            OptionAll,
+    def __init__(self, task: bool = True) -> None:
+        super().__init__(
             None,
-            None,
+            "#-#-#-#-#",
             "This task does not parse options, it bypass directly to command"
-            if description is None
-            else description,
+            if task
+            else "This action does not parse options, it bypass directly to command",
             False,
         )
