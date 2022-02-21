5662d8cff83568336d4109b7d5c9f9b447849ec2
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-21 17:00:07 -0300

    Created simple Groups for TaskIdentity

--- src/auto_flutter_dig/core/task/_unique_identity.py
+++ src/auto_flutter_dig/core/task/_unique_identity.py
@@ -5,4 +5,4 @@ __all__ = ["_TaskUniqueIdentity"]
 
 class _TaskUniqueIdentity(TaskIdentity):
     def __init__(self, task: Task) -> None:
-        super().__init__("-#-#-", "", [], lambda: task, True)
+        super().__init__("-#-#-", "-#-#-", "", [], lambda: task, True)
