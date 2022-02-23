5cb2403b0abe9c88d5b93946a9915907b9a9c82a
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-23 17:06:17 -0300

    Added __repr__ to TaskIdentity

--- src/auto_flutter_dig/model/task/identity.py
+++ src/auto_flutter_dig/model/task/identity.py
@@ -32,3 +32,14 @@ class TaskIdentity:
 
     def to_map(self) -> Tuple[TaskId, TaskIdentity]:
         return (self.id, self)
+
+    def __repr__(self) -> str:
+        return "{cls}(group={group}, id={id}, name={name}, options={options}, creator={creator}, allow_more={allow_more})".format(
+            cls=type(self).__name__,
+            group=self.group,
+            id=self.id,
+            name=self.name,
+            options=self.options,
+            creator=self.creator,
+            allow_more=self.allow_more,
+        )
