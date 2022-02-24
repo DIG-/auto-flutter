57878f73859b3098e1a1f731ae681c8181475586
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-02-24 13:56:21 -0300

    Added `parent:Subtask` in `TaskIdentity`

--- src/auto_flutter_dig/model/task/identity.py
+++ src/auto_flutter_dig/model/task/identity.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Callable, List, Tuple
+from typing import Callable, List, Optional, Tuple
 
 from ...core.utils import _Ensure, _EnsureCallable
 from ..argument.option import Option
@@ -19,6 +19,7 @@ class TaskIdentity:
         creator: Callable[[], "Task"],  # type: ignore[name-defined]
         allow_more: bool = False,  # Allow more tasks with same id
     ) -> None:
+        from .subtask import Subtask
         from .task import Task
 
         self.group: str = _Ensure.instance(group, str, "group")
@@ -29,17 +30,19 @@ class TaskIdentity:
         self.options: List[Option] = _Ensure.not_none(options, "options")
         self.creator: Callable[[], Task] = _EnsureCallable.instance(creator, "creator")
         self.allow_more: bool = _Ensure.instance(allow_more, bool, "allow_more")
+        self.parent: Optional[Subtask] = None
 
     def to_map(self) -> Tuple[TaskId, TaskIdentity]:
         return (self.id, self)
 
     def __repr__(self) -> str:
-        return "{cls}(group={group}, id={id}, name={name}, options={options}, creator={creator}, allow_more={allow_more})".format(
+        return "{cls}(id={id}, group={group}, name={name}, options={options}, creator={creator}, parent={parent}, allow_more={allow_more})".format(
             cls=type(self).__name__,
             group=self.group,
             id=self.id,
             name=self.name,
             options=self.options,
             creator=self.creator,
+            parent=type(self.parent).__name__,
             allow_more=self.allow_more,
         )
