9b3630cc973429287e72f218da099fe0a58c98fc
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-03-03 17:52:42 -0300

    Initialized Firebase module

---
+++ src/auto_flutter_dig/module/firebase/firebase.py
@@ -0,0 +1,15 @@
+from typing import Callable
+
+from ...model.task.identity import TaskIdentity
+from ...model.task.subtask import Subtask
+from ..plugin import AflutterModulePlugin
+from .task.setup.check import FirebaseCheck
+
+
+class FirebaseModulePlugin(AflutterModulePlugin):
+    @property
+    def name(self) -> str:
+        return "Firebase"
+
+    def register_setup(self, setup: Subtask, check: Callable[[TaskIdentity], None]):
+        check(FirebaseCheck.identity)
