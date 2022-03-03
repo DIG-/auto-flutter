c2c8955b8e1e18aa5069ab2ea98a7c473f51fdcd
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-03-02 23:08:30 -0300

    Implement Config Save and StackTrace Config

--- src/auto_flutter_dig/module/aflutter/task/setup/setup.py
+++ src/auto_flutter_dig/module/aflutter/task/setup/setup.py
@@ -1,7 +1,9 @@
 from .....model.task.subtask import Subtask
 from .....module.aflutter.identity import AflutterTaskIdentity
 from .....task.base.subtask_parent_task import BaseSubtaskParentTask
+from .save import AflutterSetupSaveTask
 from .show import AflutterSetupShow
+from .stack_trace import AflutterSetupStackTraceTask
 
 __all__ = ["AflutterSetupIdentity"]
 
@@ -15,7 +17,14 @@ class __AflutterSetupIdentity(AflutterTaskIdentity, Subtask):
             [],
             lambda: BaseSubtaskParentTask(self, self),
         )
-        Subtask.__init__(self, [AflutterSetupShow.identity])
+        Subtask.__init__(
+            self,
+            [
+                AflutterSetupShow.identity,
+                AflutterSetupSaveTask.identity,
+                AflutterSetupStackTraceTask.identity,
+            ],
+        )
 
 
 AflutterSetupIdentity = __AflutterSetupIdentity()
