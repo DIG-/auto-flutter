078b73a7aebce3e714660c76ce221fe728f59896
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-03-03 23:29:48 -0300

    Chain exception (E) will include traceback from origin

--- src/auto_flutter_dig/model/error/chain.py
+++ src/auto_flutter_dig/model/error/chain.py
@@ -1,3 +1,5 @@
+from sys import _getframe as sys_getframe
+from types import TracebackType
 from typing import Generic, TypeVar
 
 T = TypeVar("T", bound=BaseException)
@@ -5,12 +7,20 @@ T = TypeVar("T", bound=BaseException)
 
 class E(Generic[T]):
     def __init__(self, error: T) -> None:
-        self._error = error
+        self.__error = error
 
     @property
     def error(self) -> T:
-        return self._error
+        return self.__with_traceback(self.__error)
 
     def caused_by(self, error: BaseException) -> T:
-        self._error.__cause__ = error
-        return self._error
+        self.__error.__cause__ = error
+        return self.__with_traceback(self.__error)
+
+    def __with_traceback(self, error: T) -> T:
+        if error.__traceback__ is None:
+            frame = sys_getframe(2)
+            error.__traceback__ = TracebackType(
+                None, frame, frame.f_lasti, frame.f_lineno
+            )
+        return error
