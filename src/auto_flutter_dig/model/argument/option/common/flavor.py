38fd4edab10ac1db0fffb0129267111f30bc40c8
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-03-04 17:02:46 -0300

    Fix FlavorOption to return Flavor

--- src/auto_flutter_dig/model/argument/option/common/flavor.py
+++ src/auto_flutter_dig/model/argument/option/common/flavor.py
@@ -1,13 +1,13 @@
-from .....model.task.id import TaskId
+from .....model.project.flavor import Flavor
 from ..long_short import LongShortOptionWithValue
 from ._decoder import _DecodedOption
 
 __all__ = ["FlavorOption"]
 
 
-class FlavorOption(LongShortOptionWithValue, _DecodedOption[TaskId]):
+class FlavorOption(LongShortOptionWithValue, _DecodedOption[Flavor]):
     def __init__(self, description: str) -> None:
         super().__init__("f", "flavor", description)
 
-    def _convert(self, input: str) -> TaskId:
+    def _convert(self, input: str) -> Flavor:
         return input
