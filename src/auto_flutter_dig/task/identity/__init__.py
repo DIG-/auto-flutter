fe99aa4d5613ca077b870fc20d76b8fe5764b455
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-03-04 14:43:10 -0300

    Moved Flutter identity to new module

--- src/auto_flutter_dig/task/identity/__init__.py
+++ src/auto_flutter_dig/task/identity/__init__.py
@@ -1,5 +1,5 @@
 from ...module.aflutter.identity import AflutterTaskIdentity
 from ...module.firebase.identity import FirebaseTaskIdentity
-from .flutter import FlutterTaskIdentity
+from ...module.flutter.identity import FlutterTaskIdentity
 
 __all__ = ["AflutterTaskIdentity", "FirebaseTaskIdentity", "FlutterTaskIdentity"]
