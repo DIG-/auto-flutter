85bb1ef769d819275272e713dbe7ae8e52901491
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-03-03 22:23:07 -0300

    Removed the leading new-line from formated stack-trace

--- src/auto_flutter_dig/model/error/formater.py
+++ src/auto_flutter_dig/model/error/formater.py
@@ -6,5 +6,5 @@ from ...module.aflutter.config.const import AFLUTTER_CONFIG_ENABLE_STACK_STRACE
 
 def format_exception(error: BaseException) -> str:
     if Config.get_bool(AFLUTTER_CONFIG_ENABLE_STACK_STRACE):
-        return "".join(TracebackException.from_exception(error).format())
+        return "".join(TracebackException.from_exception(error).format()).rstrip("\n")
     return str(error)
