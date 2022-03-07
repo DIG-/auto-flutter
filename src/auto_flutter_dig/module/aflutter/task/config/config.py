c0b218c9ac441f407de691f8eb659516747d5307
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-03-07 11:58:35 -0300

    Added new config for flavor

--- src/auto_flutter_dig/module/aflutter/task/config/config.py
+++ src/auto_flutter_dig/module/aflutter/task/config/config.py
@@ -2,13 +2,14 @@ from .....model.task import *
 from .....model.task.subtask import Subtask
 from .....task.base.subtask_parent_task import BaseSubtaskParentTask
 from ...identity import AflutterTaskIdentity
+from .flavor import AflutterFlavorConfigTask
 from .platform import AflutterPlatformConfigTask
 from .refresh import AflutterConfigRefreshTask
 
 __all__ = ["AflutterConfigIdentity"]
 
 
-class __AflutterConfigIdentity(AflutterTaskIdentity, Subtask):
+class _AflutterConfigIdentity(AflutterTaskIdentity, Subtask):
     def __init__(self) -> None:
         AflutterTaskIdentity.__init__(
             self,
@@ -22,8 +23,9 @@ class __AflutterConfigIdentity(AflutterTaskIdentity, Subtask):
             [
                 AflutterConfigRefreshTask.identity,
                 AflutterPlatformConfigTask.identity,
+                AflutterFlavorConfigTask.identity,
             ],
         )
 
 
-AflutterConfigIdentity = __AflutterConfigIdentity()
+AflutterConfigIdentity = _AflutterConfigIdentity()
