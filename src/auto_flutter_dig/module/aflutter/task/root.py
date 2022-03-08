02042f9d551a149e0800bd21a12b6969d45f8a08
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-03-08 12:16:55 -0300

    Remove old dependency of old task list system

--- src/auto_flutter_dig/module/aflutter/task/root.py
+++ src/auto_flutter_dig/module/aflutter/task/root.py
@@ -1,6 +1,11 @@
 from ....core.utils import _Raise
 from ....model.task.subtask import Subtask
-from ....task._list import task_list
+from ....module.aflutter.task.config.config import AflutterConfigIdentity
+from ....module.aflutter.task.project.init.runner import ProjectInitRunnerTask
+from ....module.aflutter.task.project.read import ProjectRead
+from ....module.aflutter.task.project.save import ProjectSave
+from ....module.aflutter.task.setup import AflutterSetupIdentity
+from ....task.help import Help
 from ..identity import AflutterTaskIdentity
 
 __all__ = ["Root"]
@@ -16,7 +21,18 @@ class _AflutterRoot(AflutterTaskIdentity, Subtask):
             _Raise(AssertionError("Root does not have task")).throw,
             False,
         )
-        Subtask.__init__(self, task_list, None)
+        Subtask.__init__(
+            self,
+            [
+                Help.identity,
+                ProjectInitRunnerTask.identity,
+                AflutterConfigIdentity,
+                AflutterSetupIdentity,
+                ProjectRead.identity,
+                ProjectSave.identity,
+            ],
+            None,
+        )
 
     def __repr__(self) -> str:
         return "AflutterRoot"
