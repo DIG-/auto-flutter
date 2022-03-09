0ed73edfe5c080992b74f7792ae3236841ad3690
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-03-09 10:01:27 -0300

    Created new FlutterExecTask

---
+++ src/auto_flutter_dig/module/flutter/task/exec.py
@@ -0,0 +1,23 @@
+from ....model.argument.option import OptionAll
+from ....task.base.process import *
+from ..identity import FlutterTaskIdentity
+from .command import FlutterCommandTask
+
+
+class FlutterExecTask(FlutterCommandTask):
+    __opt_all = OptionAll()
+    identity = FlutterTaskIdentity(
+        "exec",
+        "Execute flutter command",
+        [__opt_all],
+        lambda: FlutterExecTask(),
+        allow_more=True,
+    )
+
+    def __init__(self) -> None:
+        super().__init__(describe="Executing flutter command", require_project=False)
+        self.__print_content = True  # Always print flutter exec
+
+    def _create_process(self, args: Args) -> ProcessOrResult:
+        self._command = list(args.get_all(self.__opt_all))
+        return super()._create_process(args)
